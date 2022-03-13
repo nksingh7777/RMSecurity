0987800f63ca8e2f660bfe026e13b3354743c210
nikesh <nikeshrajput777@gmail.com>
2022-03-13 14:11:42 +0530

    added model classes, next is creating persistence layer

--- src/main/java/com/application/RMSecurity/graphql/ClientQueryGraphql.java
+++ src/main/java/com/application/RMSecurity/graphql/ClientQueryGraphql.java
@@ -1,5 +1,6 @@
 package com.application.RMSecurity.graphql;
 
+import com.application.RMSecurity.model.Client;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
@@ -7,5 +8,12 @@ import org.springframework.stereotype.Component;
 public class ClientQueryGraphql implements GraphQLQueryResolver {
     public String helloWorld(){
         return "hello world";
+    }
+    public Client getClient(){
+        Client c = new Client();
+        c.setClientName("Nikesh");
+        c.setClientID("i745343");
+        c.setAddress("kings and queens PG");
+        return c;
     }
 }
